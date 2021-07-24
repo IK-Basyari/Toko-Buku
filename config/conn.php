46c9d9cdc6479debe845a3247b9025633662571a
Irvani Khoiril Basyar <87886554+IK-Basyari@users.noreply.github.com>
2021-07-25 05:22:10 +0700

    Koneksi ke database

---
+++ config/conn.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $host = "localhost";
+    $username = "root";
+    $pass = "";
+    $database = "toko_buku_harapan";
+
+    $conn = mysqli_connect($host, $username, $pass, $database);
+    
+?>
